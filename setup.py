7adb6dbaed3418740d1affb005765f493104cbba
Bouwe Westerdijk <bouwe@westerdijk.info>
2019-08-29 16:40:29 +0200

    Revert change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(
-    name='pyhaanna',
+    name='haanna',
     version='0.11.0',
     description='Plugwise Anna API to use in conjunction with Home Assistant.',
     long_description='Plugwise Anna API to use in conjunction with Home Assistant, but it can also be used without Home Assistant.',
@@ -14,7 +14,7 @@ setup(
     author='Laetificat',
     author_email='k.heruer@gmail.com',
     license='MIT',
-    packages=['pyhaanna'],
+    packages=['haanna'],
     install_requires=['requests','datetime','pytz'],
     zip_safe=False
 )
