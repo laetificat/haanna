1d95377ba6c823b9e71c977fc2bb894f8116f785
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2020-01-09 12:58:11 +0100

    Try fix 2

--- haanna/haanna.py
+++ haanna/haanna.py
@@ -122,6 +122,7 @@ class Haanna(object):
         templates = root.findall(
             ".//*[@id='{}']/template".format(schema_rule_id)
         )
+        template_id = None
         for rule in templates:
             template_id = rule.attrib["id"]
 
