6bfe1988f3e56125dd6fbedec5a336cae9fe0485
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2020-01-09 13:46:05 +0100

    Roll back fix get_rule_id_by name error? And add missing @staticmethod

--- haanna/haanna.py
+++ haanna/haanna.py
@@ -566,7 +566,8 @@ class Haanna(object):
             return root.find(locator).text
         return None
 
-    def get_rule_id_by_name(self, root, rule_name):
+    @staticmethod
+    def get_rule_id_by_name(root, rule_name):
         """Gets the rule ID based on name"""
         rules = root.findall("rule")
         for rule in rules:
