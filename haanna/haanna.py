f2d7f57f89f073c543e9c0156ec39e6b98a28008
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2020-01-09 12:37:49 +0100

    Fix get_rule_id_by name error?

--- haanna/haanna.py
+++ haanna/haanna.py
@@ -565,7 +565,7 @@ class Haanna(object):
             return root.find(locator).text
         return None
 
-    def get_rule_id_by_name(root, rule_name):
+    def get_rule_id_by_name(self, root, rule_name):
         """Gets the rule ID based on name"""
         rules = root.findall("rule")
         for rule in rules:
