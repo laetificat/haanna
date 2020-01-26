70117df50956e1a75af140f0308155ca2a027fa7
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2020-01-26 15:01:18 +0100

    Now try thermostat_temp with formatting.

--- haanna/haanna.py
+++ haanna/haanna.py
@@ -361,6 +361,7 @@ class Haanna:
         if target_temp_log_id:
             measurement = self.get_measurement_from_point_log(root, target_temp_log_id)
             value = float(measurement)
+            value = '{:.1f}'.format(round(value, 1))
             return value  
         return None
 
@@ -370,6 +371,7 @@ class Haanna:
         if thermostat_log_id:
             measurement = self.get_measurement_from_point_log(root, thermostat_log_id)
             value = float(measurement)
+            value = '{:.1f}'.format(round(value, 1))
             return value
         return None
 
